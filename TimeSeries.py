6d88582fda58943e9b1f07eb52e98bb512186385
Avery Faller <avyk37@gmail.com>
2016-02-08 16:42:09 -0500

    Fixed __repr__ method in TimeSeries

--- TimeSeries.py
+++ TimeSeries.py
@@ -15,8 +15,7 @@ class TimeSeries:
         self.data[index] = value
 	
     def __repr__(self):
-        class_name = type(self).__name__
-        return "%s(length=%r, first=%r, last=%r)" % (class_name, len(self.data), self.data[0], self.data[-1])
+        return "TimeSeries(length=%r, first=%r, last=%r)" % (len(self.data), self.data[0], self.data[-1])
     
     def __str__(self):
         return "TimeSeries: Length - %r, First - %r, Last - %r" % (len(self.data), self.data[0], self.data[-1])
