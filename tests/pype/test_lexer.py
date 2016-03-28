6b14bf786e520ac29e293fdf2cbb5270c3136e61
Avery Faller <avyk37@gmail.com>
2016-03-27 22:56:04 -0400

    Add lexer test

---
+++ tests/pype/test_lexer.py
@@ -0,0 +1,25 @@
+from pype import lexer
+from pype.lexer import tokens
+import ply
+
+def test_tokens():
+    lexer.input('(){}')
+    output = list(lexer)
+    assert(output[0].type == 'LPAREN')
+    assert(output[1].type == 'RPAREN')
+    assert(output[2].type == 'LBRACE')
+    assert(output[3].type == 'RBRACE')
+    
+def test_number():
+    lexer.input('394')
+    output = list(lexer)
+    assert(output[0].type == 'NUMBER')
+    assert(output[0].value == 394)
+    
+def test_id():
+    lexer.input('anid')
+    output = list(lexer)
+    assert(output[0].type == 'ID')
+    assert(output[0].value == 'anid')
+
+
