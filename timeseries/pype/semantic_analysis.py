df5a4b64b13d78b98d470460c27ba7c3fefc532b
haosutang <haosutang@gmail.com>
2016-03-27 21:07:29 -0400

    try travis -trial 3

--- timeseries/pype/semantic_analysis.py
+++ timeseries/pype/semantic_analysis.py
@@ -1,13 +1,13 @@
-import pype.ast as ast
+from .ast import *
 from .error import *
 
-class PrettyPrint(ast.ASTVisitor):
+class PrettyPrint(ASTVisitor):
   def __init__(self):
     pass
   def visit(self, node):
     print(node.__class__.__name__)
 
-class CheckSingleAssignment(ast.ASTVisitor):
+class CheckSingleAssignment(ASTVisitor):
   def __init__(self):
     self.component_names = []
     self.names_used = []
@@ -15,7 +15,7 @@ class CheckSingleAssignment(ast.ASTVisitor):
   def visit(self, node):
     #if the visitor visits a component, check if the name is taken. if it is not, initialize
     #an empty list for the component and insert into dictionary
-    if isinstance(node, ast.ASTComponent):
+    if isinstance(node, ASTComponent):
       self.names_used = []
       new_component_name = node.name.name
 
@@ -23,13 +23,13 @@ class CheckSingleAssignment(ast.ASTVisitor):
         raise PypeSyntaxError('Component name: ' + new_component_name +' has already been taken')
       else:
         self.component_names.append(new_component_name)
-    elif isinstance(node, ast.ASTAssignmentExpr):
+    elif isinstance(node, ASTAssignmentExpr):
       name = node.binding.name
       if name in self.names_used:
         raise PypeSyntaxError('Node name: ' + name + '  has already been taken')
       else:
         self.names_used.append(name)
-    elif isinstance(node, ast.ASTInputExpr):
+    elif isinstance(node, ASTInputExpr):
       for input_expression in node.children:
         name = input_expression.name
         if name in self.names_used:
@@ -37,36 +37,36 @@ class CheckSingleAssignment(ast.ASTVisitor):
         else:
           self.names_used.append(name)
 
-class CheckSingleIOExpression(ast.ASTVisitor):
+class CheckSingleIOExpression(ASTVisitor):
   def __init__(self):
     self.component = None
     self.component_has_input = False
     self.component_has_output = False
 
   def visit(self, node):
-    if isinstance(node, ast.ASTComponent):
+    if isinstance(node, ASTComponent):
       self.component = node.name.name
       self.component_has_input = False
       self.component_has_output = False
-    elif isinstance(node, ast.ASTInputExpr):
+    elif isinstance(node, ASTInputExpr):
       if self.component_has_input:
         raise PypeSyntaxError('Component '+str(self.component)+' has multiple input expressions')
       self.component_has_input = True
-    elif isinstance(node, ast.ASTOutputExpr):
+    elif isinstance(node, ASTOutputExpr):
       if self.component_has_output:
         raise PypeSyntaxError('Component '+str(self.component)+' has multiple output expressions')
       self.component_has_output = True
 
 '''
-class CheckUndefinedVariables(ast.ASTVisitor):
+class CheckUndefinedVariables(ASTVisitor):
   def __init__(self, symtab):
     self.symtab = symtab
     self.scope=None
 
   def visit(self, node):
-    if isinstance(node, ast.ASTComponent):
+    if isinstance(node, ASTComponent):
       self.scope = node.name.name
-    elif isinstance(node, ast.ASTID):
+    elif isinstance(node, ASTID):
       if self.symtab.lookupsym(node.name, scope=self.scope) is None:
         raise PypeSyntaxError('Undefined variable: '+str(node.name))
 '''
--- timeseries/tests/pype/test_parser.py
+++ timeseries/tests/pype/test_parser.py
@@ -3,7 +3,7 @@ import sys
 
 filename1 = '/timeseries/tests/pype/test_cases/example1.ppl'
 outputfile1 = 'test_cases/example1.ast'
-def test_example1(filename1 = './timeseries/tests/pype/test_cases/example1.ppl', outputfile1 = './timeseries/tests/pype/test_cases/example1.ast'):
+def test_example1(filename1 = 'tests/pype/test_cases/example1.ppl', outputfile1 = 'tests/pype/test_cases/example1.ast'):
   testAST = pype.TestPipeline(source=filename1)
   ASTList = testAST.return_out()
 
@@ -18,7 +18,7 @@ def test_example1(filename1 = './timeseries/tests/pype/test_cases/example1.ppl',
 
 filename2 = 'test_cases/example0.ppl'
 outputfile2 = 'test_cases/example0.ast'
-def test_example2(filename2 = './timeseries/tests/pype/test_cases/example0.ppl', outputfile2 = './timeseries/tests/pype/test_cases/example0.ast'):
+def test_example2(filename2 = 'tests/pype/test_cases/example0.ppl', outputfile2 = 'tests/pype/test_cases/example0.ast'):
   testAST = pype.TestPipeline(source=filename2)
   ASTList = testAST.return_out()
 
