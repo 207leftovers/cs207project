770dad41eabf3b0fbea5bfe17c7472c6f6ee4e79
Avery Faller <avyk37@gmail.com>
2016-04-17 16:16:10 -0400

    More optimize tests

--- tests/pype/test_optimize.py
+++ tests/pype/test_optimize.py
@@ -7,7 +7,6 @@ from pype.semantic_analysis import CheckSingleAssignment, CheckSingleIOExpressio
 from pype.optimize import *
 
 input1 = """(import timeseries)
-
 { standardize
   (input (TimeSeries t))
   (:= mu (mean t))
@@ -20,25 +19,38 @@ ast = parser.parse(input1, lexer=lexer)
 syms = ast.walk( SymbolTableVisitor() )
 ir = ast.mod_walk( LoweringVisitor(syms) )    
 
-
-
-
 def test_assignment_ellision():
-    print()
     ir.flowgraph_pass( AssignmentEllision() )
     standardize_graph = ir.graphs['standardize']
     nodes_to_have = ['@N0', '@N1', '@N3', '@N5', '@N6', '@N8']
-    for key in ir.graphs.keys():
-        print("Graph for: ", key)
-        print(ir.graphs[key].dotfile())
+    
     assert(len(standardize_graph.nodes) == len(nodes_to_have))
-    print(standardize_graph.nodes)
+    
     for node in nodes_to_have:
         assert(node in standardize_graph.nodes)
-        
+    
+    for a_var in standardize_graph.variables.keys():
+        assert(standardize_graph.variables[a_var] in nodes_to_have)
 
 def test_dead_code_elimination():
-    ir.flowgraph_pass( DeadCodeElimination() )
-    standardize_graph = ir.graphs['standardize']
-    
+    deadcodeinput = """{ component
+      (input x)
+      (:= useless 1)
+      (output x)
+    }"""
 
+    ast2 = parser.parse(deadcodeinput, lexer=lexer)
+    syms2 = ast2.walk( SymbolTableVisitor() )
+    ir2 = ast2.mod_walk( LoweringVisitor(syms2) )    
+    
+    ir2.flowgraph_pass( DeadCodeElimination() )
+    component_graph = ir2.graphs['component']
+    nodes_to_have = ['@N0', '@N3']
+
+    assert(len(component_graph.nodes) == len(nodes_to_have))
+    
+    for node in nodes_to_have:
+        assert(node in component_graph.nodes)
+
+    for a_var in component_graph.variables.keys():
+        assert(component_graph.variables[a_var] in nodes_to_have)
