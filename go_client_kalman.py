a185d2c5e2cb377b960040e27f5320c466815142
haosutang <haosutang@gmail.com>
2016-05-09 23:32:03 -0400

    Kalman filter

--- go_client_kalman.py
+++ go_client_kalman.py
@@ -2,16 +2,23 @@
 from tsdb import TSDBClient
 import timeseries as ts
 import asyncio
+import numpy as np
 
 
 async def main():
     print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
     client = TSDBClient()
 
-    await client.add_trigger('junk', 'insert_ts', None, 23)
+    await client.add_trigger('KalmanFilter', 'insert_ts', ['sig_epsilon_estimate', 'sig_eta_estimate', 'prediction'], None)
 
+    sigeta = np.random.normal(0,1,2000)
+    sigeps = np.random.normal(0,10,2000)
 
-    await client.insert_ts('one',ts.TimeSeries([1, 2, 3],[1, 4, 9]))
+    mus = np.cumsum(sigeta)+20
+    y = mus + sigeps
+
+    await client.insert_ts('one',ts.TimeSeries(np.arange(2000),y))
+    await client.upsert_meta('one', {'order': 1, 'blarg': 1})
 
 
     await client.select()
---
+++ procs/KalmanFilter.py
@@ -0,0 +1,29 @@
+# import sys
+# sys.path.append('../')
+
+import timeseries as ts
+import numpy as np
+import scipy.optimize
+
+import asyncio
+
+import set_compiler
+set_compiler.install()
+
+import pyximport
+pyximport.install()
+
+import filtering
+
+
+def proc_main(pk, row, arg):
+
+	ts1 = np.array(row['ts'].values())
+	logL_LLy = lambda x: filtering.logL_LL(x[0], x[1], ts1)
+	sigma_epsilon, sigma_eta = scipy.optimize.fmin(logL_LLy,[ts1.std()/2,ts1.std()/2])
+	print(sigma_epsilon, sigma_eta)
+	return [sigma_epsilon, sigma_eta, filtering.KF(ts1, sigma_epsilon, sigma_eta)['Predict']]
+
+
+async def main(pk, row, arg):
+    return proc_main(pk, row, arg)
