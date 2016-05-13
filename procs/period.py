0a1dfa083b8effbbb4b8cc1ed743223514bb4311
Abhishek Malali <abhishekmalali@gmail.com>
2016-05-12 21:29:56 -0400

    Tests fixed

--- procs/period.py
+++ procs/period.py
@@ -1,21 +1,13 @@
-from ._nfft import _nfft
+from ._nfft import period
 import numpy as np
 import asyncio
-
-def period(time_irr, data_irr, ofac=4):
-    fx, fy, nout = _nfft(data_irr,time_irr, ofac, 100.)  
-    max_idx = np.argmax(fy)
-    period = fx[max_idx]
-    T = 1.0 / period
-    new_time = np.mod(time_irr, 2 * T) / (2 * T)
-    return T
 
 
 def proc_main(pk, row, arg):
     rowts = row['ts']
     time_irr = rowts._times
     mag_irr = rowts._values
-    time_period = period(time_irr, mag_irr)
+    time_period = period(mag_irr, time_irr)
     return [time_period]
 
 async def main(pk, row, arg):
