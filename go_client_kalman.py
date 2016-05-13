76249879b990f82092269bc4b798eace5d97f08c
haosutang <haosutang@gmail.com>
2016-05-12 22:47:16 -0400

    go client kalman

--- go_client_kalman.py
+++ go_client_kalman.py
@@ -4,28 +4,78 @@ import timeseries as ts
 import asyncio
 import numpy as np
 
+import pyximport
+pyximport.install()
+
+import timeseries.KF as KF
+
+import matplotlib.pyplot as plt
+
 
 async def main():
-    print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
-    client = TSDBClient()
+    # print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+    # client = TSDBClient()
 
-    # Begin Transaction
-    status, tid = await client.begin_transaction()
-    assert(tid == 1)
+    # # Begin Transaction
+    # status, tid = await client.begin_transaction()
 
-    await client.add_trigger(tid, 'KalmanFilter', 'insert_ts', ['sig_epsilon_estimate', 'sig_eta_estimate'], None)#(1, 'KalmanFilter', 'insert_ts', ['sig_epsilon_estimate', 'sig_eta_estimate'], None)
+    # await client.add_trigger(tid, 'KalmanFilter', 'insert_ts', ['sig_epsilon_estimate', 'sig_eta_estimate'], None)#(1, 'KalmanFilter', 'insert_ts', ['sig_epsilon_estimate', 'sig_eta_estimate'], None)
 
-    sigeta = np.random.normal(0,1,2000)
-    sigeps = np.random.normal(0,10,2000)
+    # sigeta = np.random.normal(0,1,2000)
+    # sigeps = np.random.normal(0,10,2000)
+
+    # mus = np.cumsum(sigeta)+20
+    # y = mus + sigeps
+
+    # await client.insert_ts(tid, 'one',ts.TimeSeries(y,np.arange(2000)))#(1, 'one',ts.TimeSeries(y,np.arange(2000)))
+    # await client.upsert_meta(tid, 'one', {'order': 1, 'blarg': 1})#(1, 'one', {'order': 1, 'blarg': 1})
+
+    # status, payload = await client.select(tid)
+    # print('---------------------')
+    # print(payload)
+
+    # sig_epsilon_estimate = payload['one']['sig_epsilon_estimate']
+    # sig_eta_estimate = payload['one']['sig_eta_estimate']
+
+
+
+
+
+    sigeta = np.random.normal(0,1,2000)#scipy.stats.norm.rvs(0, 1, 2000)
+    sigeps = np.random.normal(0,10,2000)#scipy.stats.norm.rvs(0, 10, 2000)
 
     mus = np.cumsum(sigeta)+20
-    y = mus + sigeps
+    v = mus + sigeps
 
-    await client.insert_ts(tid, 'one',ts.TimeSeries(y,np.arange(2000)))#(1, 'one',ts.TimeSeries(y,np.arange(2000)))
-    await client.upsert_meta(tid, 'one', {'order': 1, 'blarg': 1})#(1, 'one', {'order': 1, 'blarg': 1})
+    t = np.arange(2000)
+    ats = ts.TimeSeries(v,t)
+        
+    # Setup Client
+    client = TSDBClient()
+    
+    # Begin Transaction
+    status, tid = await client.begin_transaction()
+    
+    # Add Trigger
+    await client.add_trigger(tid, 'KalmanFilter', 'insert_ts', ['sig_epsilon_estimate', 'sig_eta_estimate'], None)
+        
+    # Insert
+    await client.insert_ts(tid, 1, ats)
+        
+    # Select
+    status, payload = await client.select(tid, {'pk':{'==':1}}, ['sig_epsilon_estimate','sig_eta_estimate'], None)
 
-    print('---------------------')
-    await client.select(tid)
+    sig_epsilon_estimate = payload['1']['sig_epsilon_estimate']
+    sig_eta_estimate = payload['1']['sig_eta_estimate']
+
+    KFresult = KF.KF(v, sig_epsilon_estimate, sig_eta_estimate)
+    filtered = KFresult['Predict']
+
+    plt.title('Kalman Filtered')
+    plt.plot(filtered)
+    plt.show()
+
+
 
 if __name__=='__main__':
     loop = asyncio.get_event_loop()
